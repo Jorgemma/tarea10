d14cbb3155a2deaa7db7c6fce3deb6481468f320
Jorgemma <126082731+Jorgemma@users.noreply.github.com>
2023-05-07 22:03:58 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Ventana ventana =new Ventana("Layouts");
+        VentanaGrid ventanaGrid = new VentanaGrid("Ventana layout cuadricula");
+        VentanaSwing ventanaSwing=new VentanaSwing("Centigrados a Farenheid");
+    }
+}
